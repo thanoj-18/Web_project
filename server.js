1e44a509d4837d5e9de1983b4eec7c1e5a3ef990
thanoj-18 <thanojsaikrishna2008@gmail.com>
2026-08-13 17:24:13 +0530

    Before Deployment

---
+++ server.js
@@ -0,0 +1,9 @@
+const express  = require("express");
+const app = express();
+
+const port = process.env.PORT || 8080
+
+app.use(express.static('frontend'));
+app.listen(port,function(){
+    console.log("Server running on http://localhost:"+port)
+})
