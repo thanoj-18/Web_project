bd0f9d6f50eafd4b778f4215a1d878967dc573d4
thanoj-18 <thanojsaikrishna2008@gmail.com>
2026-08-14 11:10:05 +0530

    After Deployment

--- frontend/index.js
+++ frontend/index.js
@@ -1,1 +1,45 @@
-alert("Hey brooo !");
+// alert("Hey brooo !");
+
+const users = [
+    {
+        "name": "Jonn Doe",
+        "gender": "Male",
+        "image": "john.png"
+    },
+    {
+        "name": "Jane Doe",
+        "gender": "Female",
+        "image": "jane.png"
+    }
+]
+
+
+var curIndex = 0;
+
+
+function toggle() {
+    if (curIndex == 0)
+        curIndex = 1;
+    else
+        curIndex = 0;
+
+    document.getElementById("card-img").src = users[curIndex].image
+    document.getElementById("card-name").innerText = users[curIndex].name
+    document.getElementById("card-gender").innerText = users[curIndex].gender
+    //gender needs to be updated
+}
+
+function random(){
+    fetch("https://randomuser.me/api")
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+        // console.log(data)
+        var details = data.results[0];
+        document.getElementById("card-img").src = details.picture.large;
+        document.getElementById("card-gender").innerText = details.gender
+        var fullname = details.name.first + " " + details.name.last;
+        document.getElementById("card-name").innerText = fullname;
+    })
+}
